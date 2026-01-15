6a54e1eb197dac5c28e0ba4416e1224a97abd5cf
RastGame <rastgame1@gmail.com>
2026-01-15 23:38:54 +0200

    fix

--- apps/docs/.vitepress/theme/components/OutlineToggle.vue
+++ apps/docs/.vitepress/theme/components/OutlineToggle.vue
@@ -1,11 +1,13 @@
 <template>
-  <button class="outline-toggle" @click="toggleOutline" :title="isHidden ? 'Показати меню' : 'Приховати меню'">
-    <svg v-if="!isHidden" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-      <path d="M18 6L6 18M6 6l12 12"/>
-    </svg>
-    <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-      <path d="M3 12h18M3 6h18M3 18h18"/>
-    </svg>
+  <button class="outline-toggle" @click="toggleOutline" :title="isHidden ? 'Show' : 'Hide'">
+    <span class="icon-wrapper">
+      <svg v-if="!isHidden" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+        <path d="M18 6L6 18M6 6l12 12"/>
+      </svg>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+        <path d="M3 12h18M3 6h18M3 18h18"/>
+      </svg>
+    </span>
   </button>
 </template>
 
@@ -44,27 +46,24 @@ const toggleOutline = () => {
   display: flex;
   align-items: center;
   justify-content: center;
-  transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  transition: background 0.3s, color 0.3s, border-color 0.3s;
 }
 
 .outline-toggle:hover {
   background: var(--vp-c-brand-soft);
   color: var(--vp-c-brand-1);
   border-color: var(--vp-c-brand-1);
-  transform: scale(1.05);
 }
 
 .outline-toggle:active {
   transform: scale(0.95);
 }
 
+
 .outline-toggle svg {
-  transition: transform 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+  display: block;
 }
 
-.outline-toggle:hover svg {
-  transform: rotate(90deg);
-}
 
 @media (max-width: 1279px) {
   .outline-toggle {
