00a16070512b026cebf7d74af153d73d31c94b0f
RastGame <rastgame1@gmail.com>
2026-02-21 13:31:49 +0200

    fix

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -2,8 +2,6 @@ import DefaultTheme from 'vitepress/theme';
 import { h } from 'vue';
 import SidebarSelect from './components/SidebarSelect.vue';
 import TypePreview from './components/TypePreview.vue';
-import OutlineToggle from './components/OutlineToggle.vue';
-import OutlineTransform from './components/OutlineTransform.vue';
 import './custom.css';
 
 export default {
@@ -12,7 +10,6 @@ export default {
     return h(DefaultTheme.Layout, null, {
       'sidebar-nav-before': () => h(SidebarSelect),
       'layout-bottom': () => h(TypePreview),
-      'layout-top': () => [h(OutlineToggle), h(OutlineTransform)],
     });
   },
 };
